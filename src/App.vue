5b569c4d60b75ae7a2eb881ab6086087491a6bbf
Hernan Y.Ke <yuriqiao@gmail.com>
2017-05-17 11:51:34 -0400

    Add part1

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,43 @@
 <template>
   <div id="app">
+    <div>
+    <label>principal capital</label>
+    <input v-model.number="principal">
+  </div>
+  <div>
+    <label>Yearly interestRate</label>
+    <input v-model.number="interestRate">
+  </div>
+  <div>
+    <label>Investment length (timeYears)</label>
+    <input v-model.number="timeYears">
+  </div>
+  <div>
+    You will gain:
+    <output>{{final}}</output>
+  </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      principal: 0,
+      interestRate: 0,
+      timeYears: 0
+    }
+  },
+  computed: {
+    final () {
+      const P = this.principal
+      const r = this.interestRate
+      const t = this.timeYears
+      return P * Math.pow((1 + r), t)
+    }
+  }
+
 }
 </script>
 
